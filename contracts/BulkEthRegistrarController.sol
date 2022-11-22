fb4826a37680b2652f4af59dc176bb0ad1eb1b05
Ramazan Dönmez <ramazan.dnmz@gmail.com>
2022-11-22 20:24:22 +0300

    changes.

--- contracts/BulkEthRegistrarController.sol
+++ contracts/BulkEthRegistrarController.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.17;
 
 import "@ensdomains/ens-contracts/contracts/ethregistrar/ETHRegistrarController.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -41,7 +41,7 @@ contract BulkEthRegistrarController is Ownable {
     ); 
 
     address public baseControllerAddress = 0x283Af0B28c62C092C9727F1Ee09c02CA627EB7F5;
-    ETHRegistrarController private controller = ETHRegistrarController(baseControllerAddress);
+    ETHRegistrarController private _controller = ETHRegistrarController(baseControllerAddress);
 
     function updateEthRegistrarController(address controller) external onlyOwner {
         baseControllerAddress = controller;
@@ -60,11 +60,19 @@ contract BulkEthRegistrarController is Ownable {
     } 
 
     function getPrice(string calldata name, uint duration) public view returns(IPriceOracle.Price memory totalPrice) {
-        return controller.rentPrice(name, duration); 
+        return _controller.rentPrice(name, duration); 
     }
 
+    function valid(string memory name) public view returns (bool) {
+        return _controller.valid(name);
+    }
+
+    function available(string memory name) public view returns (bool) {
+       return _controller.available(name);
+    } 
+
     function getTotalPrice(string calldata name, uint duration) public view returns(uint totalPrice) {
-        IPriceOracle.Price memory price = controller.rentPrice(name, duration);
+        IPriceOracle.Price memory price = _controller.rentPrice(name, duration);
         totalPrice = price.base + price.premium;
     }
 
@@ -72,19 +80,11 @@ contract BulkEthRegistrarController is Ownable {
         prices = new EnsPrice[](query.length);
         for (uint i = 0; i < query.length; i++) {
             EnsQuery memory q = query[i];
-            IPriceOracle.Price memory price = controller.rentPrice(q.name, q.duration);
+            IPriceOracle.Price memory price = _controller.rentPrice(q.name, q.duration);
             totalPrice += price.base + price.premium;
             prices[i] = EnsPrice(q.name, msg.sender, q.duration, price);
         }
     }
-
-    function valid(string memory name) public view returns (bool) {
-        return controller.valid(name);
-    }
-
-    function available(string memory name) public view returns (bool) {
-       return controller.available(name);
-    } 
  
     function makeBulkCommitment(EnsQuery[] calldata query) external {
         delete userQueries[msg.sender];
@@ -94,8 +94,8 @@ contract BulkEthRegistrarController is Ownable {
             EnsQuery memory q = query[i];
             q.owner = msg.sender;
             q.secret = secret;
-            bytes32 commitment = controller.makeCommitment(q.name, q.owner, q.duration, secret, q.resolver, q.data, q.reverseRecord, q.fuses, q.wrapperExpiry);
-            controller.commit(commitment);
+            bytes32 commitment = _controller.makeCommitment(q.name, q.owner, q.duration, secret, q.resolver, q.data, q.reverseRecord, q.fuses, q.wrapperExpiry);
+            _controller.commit(commitment);
             userQueries[msg.sender].push( q );
         } 
     } 
@@ -113,9 +113,9 @@ contract BulkEthRegistrarController is Ownable {
         for( uint i = 0; i < queries.length; ++i ) {
             EnsQuery memory q = queries[i];
 
-            IPriceOracle.Price memory price = controller.rentPrice(q.name, q.duration);
+            IPriceOracle.Price memory price = _controller.rentPrice(q.name, q.duration);
             
-            controller.register{ value: price.base + price.premium }(q.name, q.owner, q.duration, q.secret, q.resolver, q.data, q.reverseRecord, q.fuses, q.wrapperExpiry);
+            _controller.register{ value: price.base + price.premium }(q.name, q.owner, q.duration, q.secret, q.resolver, q.data, q.reverseRecord, q.fuses, q.wrapperExpiry);
             
             emit NameRegistered(q.name, keccak256(bytes(q.name)), q.owner, price.base, price.premium, q.duration);
               
