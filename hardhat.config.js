2849b7a344cf5249821fe881a550b8950aaefc01
Ramazan Dönmez <ramazan.dnmz@gmail.com>
2022-11-30 11:24:45 +0300

    add etherscan api

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,16 @@
 
-require("dotenv").config();
-require("hardhat-gas-reporter");
+require("dotenv").config(); 
 require("@nomicfoundation/hardhat-toolbox"); 
  
-const {ALCHEMY_API_KEY, DEPLOYER_PRIVATE_KEY} = process.env;
+const {ALCHEMY_API_KEY, DEPLOYER_PRIVATE_KEY, ETHERSCAN_API_KEY} = process.env;
 
 module.exports = {
   solidity: "0.8.12",
+  etherscan: {
+    apiKey: {
+      mainnet: ETHERSCAN_API_KEY
+    }
+  },
   networks: {
     goerli: {
       url: `https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
